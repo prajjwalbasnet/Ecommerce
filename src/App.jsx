f5acd839f5ea4dc069aa2ab7519b78a4f8f2a0ad
prajjwalbasnet <prajjwal302@gmail.com>
2025-03-03 01:20:35 +1100

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import {Routes, Route} from 'react-router-dom'
+import NavBar from "./components/NavBar"
+import Home from "./pages/home"
+import Collection from "./pages/Collection"
+import Contact from "./pages/Contact"
+import Footer from "./components/Footer"
+import SearchBar from "./components/SearchBar"
+import Product from "./pages/Product"
+
+function App() {
+
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+      <NavBar/>
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/collection" element={<Collection/>} />
+        <Route path="/contact" element={<Contact/>} />
+        {/* <Route path="/about" element={<About/>}/> */}
+        <Route path="/product/:productId" element={<Product/>}/>
+        {/* <Route path="cart" element={<Cart/>} /> */}
+
+      </Routes>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
